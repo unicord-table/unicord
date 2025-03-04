6be59bd2137854366cc64559b8d5ffb0253bacf7
Karl-Louie <chthollyc@gmail.com>
2025-03-04 21:38:04 +0800

    .

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,9 +18,6 @@ const config = {
       },
     },
     extend: {
-      fontFamily: {
-        readex: ["Readex Pro", "sans-serif"],
-      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
