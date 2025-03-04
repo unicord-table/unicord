f27eac4c12e3e49a984bc92bc0d7f9fdb37b3b0e
Karl-Louie <chthollyc@gmail.com>
2025-03-04 19:14:22 +0800

    layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,34 +39,41 @@ export default function RootLayout({
         >
           <main className="min-h-screen flex flex-col items-center">
             <div className="flex-1 w-full flex flex-col gap-20 items-center">
+
               <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
                 <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
-                  <div className="flex gap-5 items-center font-semibold">
-                    <Link href={"/"}>Next.js Supabase Starter</Link>
-                    <div className="flex items-center gap-2">
-                      <DeployButton />
-                    </div>
+                  <Link className="font-readex" href={"/"}>Unicord</Link>
+                  <div className="flex flex-row gap-12">
+                    <Link className="font-readex" href={"/"}>Games</Link>
+                    <Link className="font-readex" href={"/"}>About</Link>
+                    <Link className="font-readex" href={"/"}>People</Link>
                   </div>
-                  {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
                 </div>
               </nav>
-              <div className="flex flex-col gap-20 max-w-5xl p-5">
+
+              <div className="flex flex-col gap-20 max-w-full p-5">
                 {children}
               </div>
 
-              <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
+              <footer className="w-full flex items-center justify-between border-t mx-auto text-center text-xs gap-8 p-16">
                 <p>
                   Powered by{" "}
                   <a
-                    href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
+                    href="https://github.com/unicord-table"
                     target="_blank"
                     className="font-bold hover:underline"
                     rel="noreferrer"
                   >
-                    Supabase
+                    yo mum
                   </a>
                 </p>
-                <ThemeSwitcher />
+                <div className="flex items-center gap-2">
+                  <DeployButton />
+                </div>
+                <div className="gap-4 flex">
+                  <ThemeSwitcher />
+                  {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
+                </div>
               </footer>
             </div>
           </main>
